0ceec6bc6191d2baa675742747e2f686ea276c4a
Travis Webb <me@traviswebb.com>
2014-08-15 01:42:15 -0400

    added constraint metadata to views

--- sql/views.sql
+++ sql/views.sql
@@ -13,15 +13,20 @@ select
   ordinal_position,
   col_description((view_schema || '.' || view_name)::regclass, ordinal_position),
   obj_description((view_schema || '.' || view_name)::regclass, 'pg_class'),
-  constraint_name as fkey,
-  unique_constraint_name as pkey
+  constraint_name,
+  constraint_type,
+  unique_constraint_name,
+  position_in_unique_constraint
 
 from information_schema.columns
+natural full join information_schema.views
 natural full join information_schema.constraint_column_usage
 natural full join information_schema.key_column_usage
-natural full join information_schema.referential_constraints
 natural full join information_schema.view_column_usage
-natural full join information_schema.views
+natural full join information_schema.view_table_usage
+natural full join information_schema.table_constraints
+natural full join information_schema.referential_constraints
 
 where view_schema not in ('information_schema', 'pg_catalog')
+and column_name is not null
 order by view_name
